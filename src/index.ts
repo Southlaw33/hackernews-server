8db71be8f0deea3cf90a8393fa57262ae1b86eb0
Atchutha Rao T A <atchutha57@gmail.com>
2025-03-26 14:47:26 +0530

    Add dotenv dependency and implement authentication routes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import "dotenv/config";
+import "dotenv/config";
+import { serve } from "@hono/node-server";
+import { allRoutes } from "./routes/routes";
+import { Hono } from "hono";
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(allRoutes, (info) => {
+  console.log(`server is running on port ${info.port}`);
+});
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,46 +1,20 @@
 import { Hono } from "hono";
+import { authenticationRoutes } from "./auth-routes";
+import { prisma } from "../extras/prisma";
+import jwt from "jsonwebtoken";
+import { jwtSecretKey } from "../../environment";
+import { logger } from "hono/logger";
 
-import {
-  LogInWithUsernameAndPassword,
-  signUpWithUsernameAndPassword,
-} from "../controllers/authentication/authentication-controller";
-import {
-  LogInWithUsernameAndPasswordError,
-  SignUpWithUsernameAndPasswordError,
-} from "../controllers/authentication/+types";
+export const allRoutes = new Hono();
 
-export const authenticationRoutes = new Hono();
+allRoutes.route("/authentication", authenticationRoutes);
+allRoutes.use(logger());
 
-authenticationRoutes.post("/sign-up", async (context) => {
-  const { username, password } = await context.req.json();
-  try {
-    const result = await signUpWithUsernameAndPassword({
-      username,
-      password,
-    });
-    return context.json(
-      {
-        data: result,
-        message: "all ok",
-      },
-      201
-    );
-  } catch (e) {
-    if (e === SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME) {
-      return context.json(
-        {
-          message: "USername is already existing",
-        },
-        409
-      );
-    }
-    if (e === SignUpWithUsernameAndPasswordError.UNKNOWN) {
-      return context.json(
-        {
-          message: "Server error",
-        },
-        500
-      );
-    }
-  }
+allRoutes.get("/health", (context) => {
+  return context.json(
+    {
+      message: "All ok",
+    },
+    200
+  );
 });
