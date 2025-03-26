08d10f1652b17b6612ac3681196b77840770fba6
Atchutha Rao T A <atchutha57@gmail.com>
2025-03-26 10:41:07 +0530

    Add Sign-up with username and password, update User model

---
+++ src/controllers/authentication/authentication-controller.ts
@@ -0,0 +1,60 @@
+import type { User } from "@prisma/client";
+import { Sign } from "crypto";
+import { createHash } from "crypto";
+import { prisma } from "../../extras/prisma";
+import {
+  LogInWithUsernameAndPasswordError,
+  SignUpWithUsernameAndPasswordError,
+  type LogInWithUsernameAndPasswordResult,
+  type SignUpWithUsernameAndPasswordResult,
+} from "./+types";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client/extension";
+import { jwtSecretKey } from "../../../environment";
+
+const createPasswordHash = (parameters: { password: string }): string => {
+  return createHash("sha256").update(parameters.password).digest("hex");
+};
+
+export const signUpWithUsernameAndPassword = async (parameters: {
+  username: string;
+  password: string;
+}): Promise<SignUpWithUsernameAndPasswordResult> => {
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        username: parameters.username,
+      },
+    });
+
+    if (existingUser) {
+      throw SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME;
+    }
+    const passwordHash = createPasswordHash({
+      password: parameters.password,
+    });
+    const user = await prisma.user.create({
+      data: {
+        username: parameters.username,
+        password: passwordHash,
+      },
+    });
+    const JwtPayload: jwt.JwtPayload = {
+      iss: "atchutha57@gmail.com",
+      sub: user!.id,
+      username: user!.username,
+    };
+    const token = jwt.sign(JwtPayload, jwtSecretKey, {
+      expiresIn: "30d",
+    });
+
+    const result: SignUpWithUsernameAndPasswordResult = {
+      token,
+      user,
+    };
+    return result;
+  } catch (e) {
+    console.log("error", e);
+    throw SignUpWithUsernameAndPasswordError.UNKNOWN;
+  }
+};
