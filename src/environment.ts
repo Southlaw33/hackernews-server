1eaa49eddf9f965813c119f64e9bd29a477130aa
Atchutha Rao T A <atchutha57@gmail.com>
2025-04-04 16:34:54 +0530

    Enhance environment variable handling by adding error messages for missing JWT_SECRET_KEY, DATABASE_URL, and DIRECT_URL

--- src/environment.ts
+++ src/environment.ts
@@ -1,2 +1,18 @@
-//this is just to create a jwtsecretkey access accross the project
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || process.exit(1);
+export const JWT_SECRET_KEY =
+  process.env.JWT_SECRET_KEY ||
+  (() => {
+    console.error("JWT_SECRET_KEY is not set! Exiting...");
+    process.exit(1);
+  })();
+export const databaseUrl =
+  process.env.DATABASE_URL ||
+  (() => {
+    console.error("DATABASE_URL is not set! Exiting...");
+    process.exit(1);
+  })();
+export const directUrl =
+  process.env.DIRECT_URL ||
+  (() => {
+    console.error("DIRECT_URL is not set! Exiting...");
+    process.exit(1);
+  })();
