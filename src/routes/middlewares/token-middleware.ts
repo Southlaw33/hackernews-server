ee71e2943c027dacce0ef0d271ed1f0b56bf7666
Atchutha Rao T A <atchutha57@gmail.com>
2025-04-05 12:49:02 +0530

    Add OpenAPI documentation and update dependencies for improved API structure

--- src/routes/middlewares/token-middleware.ts
+++ src/routes/middlewares/token-middleware.ts
@@ -1,25 +1,30 @@
 import { createMiddleware } from "hono/factory";
-import { JWT_SECRET_KEY} from "../../environment.js";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET_KEY } from "../../environment.js";
 
 export const tokenMiddleWare = createMiddleware<{
   Variables: {
     userId: string;
   };
 }>(async (context, next) => {
-  const token = context.req.header("token");
-  if (!token) {
-    return context.json({ message: "Token missing , Unauthorized" }, 401);
+  const authHeader = context.req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return context.json({ message: "missing Token" }, 401);
   }
+
+  const token = authHeader.split(" ")[1];
+
   try {
-    const verified = jwt.verify(token, JWT_SECRET_KEY) as jwt.JwtPayload;
-    const userId = verified.sub;
+    const payload = jwt.verify(token, JWT_SECRET_KEY) as jwt.JwtPayload;
+    const userId = payload.sub;
+
     if (userId) {
       context.set("userId", userId);
     }
-    console.log(verified);
+
     await next();
   } catch (e) {
-    return context.json({ message: "Invalid token" }, 401);
+    return context.json({ message: "Unauthorized token" }, 401);
   }
 });
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,15 +2,18 @@ import { Hono } from "hono";
 import { authenticationRoutes } from "./auth-routes.js";
 import { usersRoutes } from "./user-routes.js";
 import { postsRoutes } from "./post-routes.js";
-import { likeRoutes } from "../routes/like-routes.js";
+import { likeRoutes } from "./like-routes.js";
 import { commentRoutes } from "./comment-routes.js";
-import { logger } from "hono/logger";
+import { openapi } from "../docs/openapi.js";
+import { swaggerUI } from "@hono/swagger-ui";
 
 export const allRoutes = new Hono();
-allRoutes.use(logger());
 
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
 allRoutes.route("/posts", postsRoutes);
 allRoutes.route("/likes", likeRoutes);
 allRoutes.route("/comments", commentRoutes);
+
+allRoutes.get("/doc", (c) => c.json(openapi));
+allRoutes.get("/ui", swaggerUI({ url: "/doc" }));
