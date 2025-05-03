04296765c29b73b296525f50c5e718423ddaf74b
Atchutha Rao T A <atchutha57@gmail.com>
2025-05-04 01:21:31 +0530

    New updartes to the authentication - better-auth for session based authentication ; and relevant changes on the other files

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,19 +1,29 @@
 import { Hono } from "hono";
-import { authenticationRoutes } from "./auth-routes.js";
-import { usersRoutes } from "./user-routes.js";
-import { postsRoutes } from "./post-routes.js";
-import { likeRoutes } from "./like-routes.js";
-import { commentRoutes } from "./comment-routes.js";
-import { openapi } from "../docs/openapi.js";
-import { swaggerUI } from "@hono/swagger-ui";
+import { authenticationRoutes } from "./auth/auth-routes";
+import { usersRoutes } from "./users/user-routes";
+import { postsRoutes } from "./posts/post-routes";
+import { likesRoutes } from "./likes/like-routes";
+import { commentsRoutes } from "./comments/comment-routes";
+import { authRoute } from "./middlewares/session-middleware";
+import { cors } from "hono/cors";
 
 export const allRoutes = new Hono();
 
+allRoutes.use(
+  cors({
+    origin: "http://localhost:4000",
+
+    allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowHeaders: ["Content-Type", "Authorization", "token"],
+    exposeHeaders: ["Set-Cookie"],
+    maxAge: 600,
+  })
+);
+
+allRoutes.route("/api/auth", authRoute);
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
 allRoutes.route("/posts", postsRoutes);
-allRoutes.route("/likes", likeRoutes);
-allRoutes.route("/comments", commentRoutes);
-
-allRoutes.get("/doc", (c) => c.json(openapi));
-allRoutes.get("/ui", swaggerUI({ url: "/doc" }));
+allRoutes.route("/likes", likesRoutes);
+allRoutes.route("/comments", commentsRoutes);
