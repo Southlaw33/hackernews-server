b722c89f674db1d642ac29d0d7ee0be65058ab9e
Atchutha Rao T A <atchutha57@gmail.com>
2025-05-04 01:24:07 +0530

    added a comment

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import { serve } from "@hono/node-server";
 import { allRoutes } from "./routes/routes.js";
-
+//main index
 serve(allRoutes, (info) => {
   console.log(`SERVER RUNNING ON PORT ${info.port}`);
 });
+
